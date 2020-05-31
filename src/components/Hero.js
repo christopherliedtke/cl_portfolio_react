01dd2da3fae3f897dbe5747f924d1e68f9333f7f
ggwoodsCL <ggwoods@gmx.de>
2020-05-31 10:53:36 +0200

    Fix hero lang change for typist

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 import { withTranslation } from 'react-i18next';
@@ -7,38 +7,51 @@ import { Zoom, Fade } from 'react-reveal';
 import Typist from 'react-typist';
 
 function Hero({ t }) {
+    const [typist, setTypist] = useState();
+
+    useEffect(() => {
+        setTypist('');
+        setTimeout(() => {
+            setTypist(repeatTypist());
+        }, 100);
+    }, [t]); // eslint-disable-line
+
+    const repeatTypist = () => {
+        return (
+            <Typist
+                avgTypingDelay={80}
+                stdTypingDelay={50}
+                cursor={{
+                    show: true,
+                    blink: true,
+                    element: '|',
+                    hideWhenDone: true,
+                    hideWhenDoneDelay: 1000,
+                }}
+            >
+                <Typist.Delay ms={2000} />
+                Hi! <strong>{t('hero.me')}</strong>
+                <Typist.Delay ms={500} />
+                <br />
+                <Typist.Delay ms={1500} />
+                <span className="highlight">{t('hero.desc')}</span>
+                <Typist.Delay ms={800} />
+                <br /> {t('hero.why')}.
+            </Typist>
+        );
+    };
+
     return (
         <div className="home hero container-fuid" id="home">
             <div className="container">
                 <div className="row">
-                    <div className="col-12 col-lg-6 d-flex justify-content-center align-items-center">
+                    <div className="col-12 col-md-6 d-flex justify-content-center align-items-center">
                         <Zoom left duration={2000}>
                             <img className="m-4" src="/img/christopherliedtke_filter_500x500.jpg" alt="" />
                         </Zoom>
                     </div>
-                    <div className="col-12 col-lg-6 d-flex flex-column justify-content-center p-5 p-lg-2">
-                        <h1>
-                            <Typist
-                                avgTypingDelay={80}
-                                stdTypingDelay={50}
-                                cursor={{
-                                    show: true,
-                                    blink: true,
-                                    element: '|',
-                                    hideWhenDone: true,
-                                    hideWhenDoneDelay: 1000,
-                                }}
-                            >
-                                <Typist.Delay ms={2000} />
-                                Hi! <strong>{t('hero.me')}</strong>
-                                <Typist.Delay ms={500} />
-                                <br />
-                                <Typist.Delay ms={1500} />
-                                <span className="highlight">{t('hero.desc')}</span>
-                                <Typist.Delay ms={800} />
-                                <br /> {t('hero.why')}.
-                            </Typist>
-                        </h1>
+                    <div className="col-12 col-md-6 d-flex flex-column justify-content-center align-items-center align-items-md-stretch p-5 p-md-2">
+                        <h1>{typist}</h1>
                         <Fade bottom delay={11000}>
                             <Link to="/#projekte">
                                 <button className="btn btn-primary mt-4">{t('hero.myProjectsBtn')}</button>
